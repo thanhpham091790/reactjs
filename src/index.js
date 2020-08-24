b6379b5acc8f78aa8fea93111ea623a0d98224b7
Thanh Pham <thanhpham091790@gmail.com>
2020-08-24 13:00:52 -0600

    Render Function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const myfirstelement = <h1>Hello React!</h1>
-ReactDOM.render(myfirstelement,document.getElementById('root'));
+ReactDOM.render(<p>Hello Thanh!</p>,document.getElementById("root"));
