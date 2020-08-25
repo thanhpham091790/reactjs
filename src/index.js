23e4b9aacd2240a8f84a4934e410b854afc6a450
Thanh Pham <thanhpham091790@gmail.com>
2020-08-25 13:02:33 -0600

    Creat a class component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const myelement = (
-    <input type="text" />
-);
-ReactDOM.render(myelement, document.getElementById("root"));
+class Car extends React.Component{
+    render(){
+        return <h2>Hi, I am a Car!</h2>;
+    }
+}
+
+ReactDOM.render(<Car />, document.getElementById("root"));
