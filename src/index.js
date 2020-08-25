b8a17411c9e7c37e66eb3bb05f4f179935d4f313
Thanh Pham <thanhpham091790@gmail.com>
2020-08-25 11:44:59 -0600

    The HTML Code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<p>Hello Thanh!</p>,document.getElementById("root"));
+const myelement = (
+    <table>
+        <tr>
+            <th>Name</th>
+        </tr>
+        <tr>
+            <th>John</th>
+        </tr>
+        <tr>
+            <td>Pham</td>
+        </tr>
+    </table>
+);
+ReactDOM.render(myelement, document.getElementById("root"));
