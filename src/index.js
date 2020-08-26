1318aca23780892eaadec35c55ed4d608f01023c
Thanh Pham <thanhpham091790@gmail.com>
2020-08-26 13:38:52 -0600

    Components in Files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Car from './App.js';
 
-class Car extends React.Component {
-    render() {
-        return <h2>I am a Car!</h2>
-    }
-}
-
-class Garage extends React.Component{
-    render(){
-        return(
-            <div>
-                <h1>Who lives in my Garage?</h1>
-                <Car />
-            </div>
-        );
-    }
-}
-ReactDOM.render(<Garage />, document.getElementById("root"));
+ReactDOM.render(<Car />, document.getElementById("root"));
