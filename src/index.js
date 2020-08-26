49a04df1822eb5d59e63d287b30b856331c63eb5
Thanh Pham <thanhpham091790@gmail.com>
2020-08-26 13:53:44 -0600

    Pass Data

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Car extends React.Component{
-    render(){
-    return <h2>I'm a {this.props.color} {this.props.brand} Car!</h2>
+class Car extends React.Component {
+    render() {
+        return <h2>I'm a {this.props.color} {this.props.brand} Car!</h2>
     }
 }
 
-ReactDOM.render(<Car color="silver" brand="Toyota" />, document.getElementById("root"));
+class Garage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Who lives in my Garage ?</h1>
+                <Car color="white" brand="Acura" />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Garage />, document.getElementById("root"));
