fdcf49b9af1e18b64459bb2753f4176d6da0d0e7
Thanh Pham <thanhpham091790@gmail.com>
2020-08-26 13:47:36 -0600

    React Props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class Car extends React.Component{
   render(){
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Car from './App.js';
 
-ReactDOM.render(<Car />, document.getElementById("root"));
+class Car extends React.Component{
+    render(){
+    return <h2>I'm a {this.props.color} {this.props.brand} Car!</h2>
+    }
+}
+
+ReactDOM.render(<Car color="silver" brand="Toyota" />, document.getElementById("root"));
