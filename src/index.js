5f69da462637e32c0f5a7b1195d649f60d36bdaa
Thanh Pham <thanhpham091790@gmail.com>
2020-08-26 13:22:04 -0600

    Component Constructor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Car(){
-    return <h2>Hi, I'm also a Car!</h2>
+class Car extends React.Component{
+    constructor(){
+        super();
+        this.state = {color:"silver"};
+    }
+    render(){
+    return <h2>I am a {this.state.color} Car!</h2>
+    }
 }
-
 ReactDOM.render(<Car />, document.getElementById("root"));
