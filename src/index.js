8b848b48f22bc35b32b45e569e4b616d269242a0
Thanh Pham <thanhpham091790@gmail.com>
2020-08-28 07:16:39 -0600

    render

--- src/index.js
+++ src/index.js
@@ -2,20 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Header extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { favoritecolor: "silver" }
-    }
-
-    static getDerivedStateFromProps(props, state){
-        return ({favoritecolor:props.favcol});
-    }
 
     render() {
         return (
-            <h1>My favorite color is {this.state.favoritecolor}.</h1>
+            <h1>This is the content of the Header component.</h1>
         );
     }
 }
 
-ReactDOM.render(<Header favcol="yellow" />, document.getElementById("root"));
+ReactDOM.render(<Header />, document.getElementById("root"));
