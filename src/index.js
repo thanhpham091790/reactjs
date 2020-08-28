dccddfe5e7635a3e26fea508b06be34ec802e08d
Thanh Pham <thanhpham091790@gmail.com>
2020-08-28 07:00:23 -0600

    Constructor

--- src/index.js
+++ src/index.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Car extends React.Component {
-    constructor(props) {
+class Header extends React.Component{
+    constructor(props){
         super(props);
-        this.state = {
-            brand: "Toyota",
-            model: "Camry",
-            color: "Silver",
-            year: "2017"
-        };
+        this.state = {favoritecolor:"silver"}
     }
-
-    changeColor = () =>{
-        this.setState({
-            color:"Black"
-        });
-    }
-
+    
     render(){
         return(
-            <div>
-                <h1>My {this.state.brand}</h1>
-                <p>
-                    It is a {this.state.color} {this.state.model} from {this.state.year}.
-                </p>
-                <button type="button" onClick={this.changeColor}>Change color</button>
-            </div>
+        <h1>My favorite color is {this.state.favoritecolor}.</h1>
         );
     }
 }
 
-ReactDOM.render(<Car model="Mustang" />, document.getElementById("root"));
+ReactDOM.render(<Header />, document.getElementById("root"));
