16846967fa5a47c5934a477940d8e050ad90faef
Thanh Pham <thanhpham091790@gmail.com>
2020-08-28 07:56:57 -0600

    ComponentDidUpdated - Updating Phase

--- src/index.js
+++ src/index.js
@@ -14,20 +14,15 @@ class Header extends React.Component {
         },3000);
     }
 
-    getSnapshotBeforeUpdate(prevprops, prestate){
-        document.getElementById("div1").innerHTML = "Before updated favorite color is " + prestate.favoritecolor;
-    }
-
     componentDidUpdate(){
-        document.getElementById("div2").innerHTML = "After updated favorite color is " + this.state.favoritecolor;
+        document.getElementById("mydiv").innerHTML = "The updated favorite color is " + this.state.favoritecolor;
     }
    
     render() {
         return (
             <div>
                 <h1>My favorite color is {this.state.favoritecolor}.</h1>
-                <div id="div1"></div>
-                <div id="div2"></div>
+                <div id="mydiv"></div>
             </div>
         );
     }
