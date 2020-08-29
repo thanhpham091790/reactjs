1d2948db35b96a483d427f14fd73c3e852608956
Thanh Pham <thanhpham091790@gmail.com>
2020-08-29 07:08:36 -0600

    Bind the event handler to this - Passing Argument

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 
 class Football extends React.Component{
    
-    shoot = (a) =>{
+    shoot(a){
         alert(a);
     }
+
     render(){
         return(
-            <button onClick={() => this.shoot("Goal!!!")}>Take the shot!</button>
+            <button onClick={this.shoot.bind(this, "Goal!!!")}>Take the shot!</button>
         );
     }
 }
