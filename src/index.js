04531011fae84b1179da16fbe1f37e8f5e6e22a2
Thanh Pham <thanhpham091790@gmail.com>
2020-08-29 06:58:18 -0600

    Event Handler

--- src/index.js
+++ src/index.js
@@ -1,42 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Container extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            show: true
-        };
+class Football extends React.Component{
+    shoot = () =>{
+        alert("Good Shot!");
     }
-
-    delHeader = () =>{
-        this.setState({show: false});
-    }
-
     render(){
-        let myheader;
-        if(this.state.show){
-            myheader = <Child />;
-        }
         return(
-            <div>
-                {myheader}
-                <button type="button" onClick={this.delHeader}>Delete Header</button>
-            </div>
+            <button onClick={this.shoot}>Take the shot!</button>
         );
     }
 }
-
-class Child extends React.Component{
-    componentWillUnmount(){
-        alert("The component named Header is about to be unmounted.");
-    }
-
-    render(){
-        return(
-            <h1>Hello Pham Hong Thanh!</h1>
-        );
-    }
-}
-
-ReactDOM.render(<Container />, document.getElementById("root"));
+ReactDOM.render(<Football />, document.getElementById("root"));
