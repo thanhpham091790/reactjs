3d6d0d84f5ccc34db7c19e71e425e1d24a1e74e6
Thanh Pham <thanhpham091790@gmail.com>
2020-08-30 07:54:20 -0600

    Css module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './App.module.css';
 
 class Car extends React.Component{
   render(){
-    return <h2>Hi, I am a Toyota Car!</h2>
+    return <h2 className={styles.bigblue}>Hi, I am a Toyota Car!</h2>
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import Car from './App.js';
 
-class MyHeader extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello Style!</h1>
-                <p>Add a little style!</p>
-            </div>
-        );
-    }
-}
-
-
-ReactDOM.render(<MyHeader />, document.getElementById("root"));
+ReactDOM.render(<Car />, document.getElementById("root"));
