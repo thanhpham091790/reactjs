f994850c3082ec6b3486939e199e56a828ac3468
Thanh Pham <thanhpham091790@gmail.com>
2020-08-30 07:31:14 -0600

    Inline Styling

--- src/index.js
+++ src/index.js
@@ -1,69 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MyForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            age: null,
-            quote: 'Hello world! My name is Pham Hong Thanh',
-            car: "Volvo",
-            errormessage: ''
-        };
-    }
-
-    myChangeHandler = (event) => {
-        let nam = event.target.name;
-        let val = event.target.value;
-        let err = '';
-
-        if (nam == 'age') {
-            if (!Number(val) && val != '') {
-                err = <strong>Your age must be a number</strong>;
-            }
-        }
-        this.setState({
-            errormessage: err
-        });
-        this.setState({
-            [nam]: val
-        });
-    }
-
-    mySubmitHandler = (event) => {
-        event.preventDefault();
-        alert("You are submitting " + this.state.username);
-    }
-
-    render() {
-        let header = '';
-        if (this.state.username && this.state.age) {
-            header = <h1>Hello {this.state.username} {this.state.age}</h1>;
-        } else {
-            header = '';
-        }
-        return (
-            <form onSubmit={this.mySubmitHandler}>
-                {header}
-                <p>Enter your name:</p>
-                <input type="text" name="username" onChange={this.myChangeHandler} />
-                <p>Enter your age:</p>
-                <input type="text" name="age" onChange={this.myChangeHandler} />
-                {this.state.errormessage}
-                <p>Enter your quote:</p>
-                <textarea value={this.state.quote} />
-                <p>Enter your car:</p>
-                <select value={this.state.car}>
-                    <option value="Ford">Ford</option>
-                    <option value="Volvo">Volvo</option>
-                    <option value="Fiat">Fiat</option>
-                </select>
-                <p></p>
-                <input type="submit" value="Submit" />
-            </form>
+class MyHeader extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1 style={{color:"red"}}>Hello Style!</h1>
+                <p>Add a little style!</p>
+            </div>
         );
     }
 }
 
-ReactDOM.render(<MyForm />, document.getElementById("root"));
+ReactDOM.render(<MyHeader />, document.getElementById("root"));
