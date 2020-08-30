d9972c910ef50e996a334c0d0b69ee128d26964e
Thanh Pham <thanhpham091790@gmail.com>
2020-08-30 06:57:18 -0600

    Conditional Rendering

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MyForm extends React.Component{
-    constructor(props){
+class MyForm extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            username:''
+            username: ''
         };
     }
 
-    myChangeHandler = (event) =>{
+    myChangeHandler = (event) => {
         this.setState({
             username: event.target.value
         });
     }
 
-    render(){
-        return(
+    render() {
+        let header = '';
+        if (this.state.username) {
+            header = <h1>Hello {this.state.username}</h1>;
+        }else{
+            header = '';
+        }
+        return (
             <form>
-                <h1>Hello {this.state.username}</h1>
+                {header}
                 <p>Enter your name:</p>
                 <input type="text" onChange={this.myChangeHandler} />
             </form>
