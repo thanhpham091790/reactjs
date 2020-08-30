b68c53eb00848b9790e65dfec7bb95f593760a98
Thanh Pham <thanhpham091790@gmail.com>
2020-08-30 07:35:01 -0600

    Javascript Object

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MyHeader extends React.Component{
-    render(){
-        return(
+class MyHeader extends React.Component {
+    render() {
+        const mystyle = {
+            color: "white",
+            backgroundColor: "DodgerBlue",
+            padding: "10px",
+            fontFamily: "Arial"
+        };
+        return (
             <div>
-                <h1 style={{backgroundColor:"lightblue"}}>Hello Style!</h1>
+                <h1 style={mystyle}>Hello Style!</h1>
                 <p>Add a little style!</p>
             </div>
         );
