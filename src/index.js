e8a564834bc94cf47a181bdcf7cb0f7ec9d4b735
Thanh Pham <thanhpham091790@gmail.com>
2020-08-30 07:45:14 -0600

    CSS Stylesheet

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
 
 class MyHeader extends React.Component {
     render() {
-        const mystyle = {
-            color: "white",
-            backgroundColor: "DodgerBlue",
-            padding: "10px",
-            fontFamily: "Arial"
-        };
         return (
             <div>
-                <h1 style={mystyle}>Hello Style!</h1>
+                <h1>Hello Style!</h1>
                 <p>Add a little style!</p>
             </div>
         );
     }
 }
+
 
 ReactDOM.render(<MyHeader />, document.getElementById("root"));
