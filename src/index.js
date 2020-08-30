0f80e063e86bc2f7fa7bc45206fe7e12a4e547ab
Thanh Pham <thanhpham091790@gmail.com>
2020-08-30 07:32:31 -0600

    camelCased Property Names

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ class MyHeader extends React.Component{
     render(){
         return(
             <div>
-                <h1 style={{color:"red"}}>Hello Style!</h1>
+                <h1 style={{backgroundColor:"lightblue"}}>Hello Style!</h1>
                 <p>Add a little style!</p>
             </div>
         );
