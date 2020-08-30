c4ddd3a0a1d6b77a460ce91b7ce93387ed334ee2
Thanh Pham <thanhpham091790@gmail.com>
2020-08-30 06:53:38 -0600

    Handing Form

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class MyForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            username:''
+        };
+    }
+
+    myChangeHandler = (event) =>{
+        this.setState({
+            username: event.target.value
+        });
+    }
+
     render(){
         return(
             <form>
-                <h1>Hello</h1>
+                <h1>Hello {this.state.username}</h1>
                 <p>Enter your name:</p>
-                <input type="text" />
+                <input type="text" onChange={this.myChangeHandler} />
             </form>
         );
     }
