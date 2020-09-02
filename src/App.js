4c40d0419f821c6f098441c90a0e07fc6a70e7f8
Thanh Pham <thanhpham091790@gmail.com>
2020-09-02 15:20:58 -0600

    React Sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './App.module.css';
+import './mysass.scss';
 
-class Car extends React.Component{
-  render(){
-    return <h2 className={styles.bigblue}>Hi, I am a Toyota Car!</h2>
+class Car extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Who are you ?</h1>
+        <h2 className={styles.bigblue}>Hi, I am a Toyota Car!</h2>
+      </div>
+    );
+
   }
 }
 
