6438bf8d72273fd650282140e03b40101656da31
Thanh Pham <thanhpham091790@gmail.com>
2021-02-04 19:39:20 -0700

    Expressions in JSX

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 
 const myelement = <h1>I love Jsx!</h1>;
 const myelement1 = React.createElement("h1",{},"I do not use jxs!");
-ReactDOM.render(myelement1,document.getElementById("root"));
+const myelement2 = <h1>React is {5+5} times better with JSX!</h1>
+ReactDOM.render(myelement2,document.getElementById("root"));
