17d594b1318b84e8cc86ff7fdd2db538d3081391
Thanh Pham <thanhpham091790@gmail.com>
2021-02-04 18:23:19 -0700

    Class and class inheritance

--- myfirstreact/src/react_get_started.js
+++ myfirstreact/src/react_get_started.js
@@ -2,3 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const myfirstelement = <h1>Thanh H Pham</h1>;
+
+ReactDOM.render(
+  myfirstelement,
+  document.getElementById('root')
+);
