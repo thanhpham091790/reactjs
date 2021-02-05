801030befd895c118bab2d14e879f9266d3d7c28
Thanh Pham <thanhpham091790@gmail.com>
2021-02-04 20:51:13 -0700

    Create a class component

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const myelement = <h1>I love Jsx!</h1>;
-const myelement1 = React.createElement("h1",{},"I do not use jxs!");
-const myelement2 = <h1>React is {5+5} times better with JSX!</h1>
-const myelement3 = (
-    <ul>
-        <li>Apples</li>
-        <li>Bananas</li>
-        <li>Cherries</li>
-    </ul>
-);
-const myelement4 = (
-    <div>
-        <h1>I am a Header</h1>
-        <h1>I am a Header too.</h1>
-    </div>
-);
-const myelement5 = <input type="text" />;
-ReactDOM.render(myelement5,document.getElementById("root"));
+class Car extends React.Component{
+    render(){
+        return <h2>Hi, I am a car!</h2>;
+    }
+}
+
+ReactDOM.render(<Car/>,document.getElementById("root"));
