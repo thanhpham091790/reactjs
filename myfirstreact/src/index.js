cd935f46ac0e6fa678ca1835fee2865a71c32a4e
Thanh Pham <thanhpham091790@gmail.com>
2021-02-04 21:00:06 -0700

    Component constructor

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Car extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            color: "red"
+        };
+    }
     render(){
-        return <h2>Hi, I am a car!</h2>;
+        return <h2>Hi, I am a {this.state.color} car!</h2>;
     }
 }
 
@@ -11,4 +17,4 @@ function Car1(){
     return <h2>Hi, I am a car1!</h2>;
 }
 
-ReactDOM.render(<Car1 />,document.getElementById("root"));
+ReactDOM.render(<Car />,document.getElementById("root"));
