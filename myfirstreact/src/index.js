3ab8d889359b75fff9f0dd1c5312f7391c3156d7
Thanh Pham <thanhpham091790@gmail.com>
2021-02-04 19:43:41 -0700

    Inserting a large block of HTML

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -4,4 +4,11 @@ import ReactDOM from 'react-dom';
 const myelement = <h1>I love Jsx!</h1>;
 const myelement1 = React.createElement("h1",{},"I do not use jxs!");
 const myelement2 = <h1>React is {5+5} times better with JSX!</h1>
-ReactDOM.render(myelement2,document.getElementById("root"));
+const myelement3 = (
+    <ul>
+        <li>Apples</li>
+        <li>Bananas</li>
+        <li>Cherries</li>
+    </ul>
+);
+ReactDOM.render(myelement3,document.getElementById("root"));
