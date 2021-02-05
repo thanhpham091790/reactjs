e05c9620cbfb6a875130ac7096def37762ca8fd4
Thanh Pham <thanhpham091790@gmail.com>
2021-02-05 14:39:13 -0700

    Lifecycle of component -  componentDidMount()

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -8,15 +8,22 @@ class Header extends React.Component{
             favoritecolor: "red"
         };
     }
-    static getDerivedStateFromProps(props, state){
-        return {
-            favoritecolor: props.favcol
-        };
-    }
+    // static getDerivedStateFromProps(props, state){
+    //     return {
+    //         favoritecolor: props.favcol
+    //     };
+    // }
     render(){
         return(
             <h1>My favorite color is {this.state.favoritecolor}</h1>
         );
+    }
+    componentDidMount(){
+        setTimeout(()=>{
+            this.setState({
+                favoritecolor: "yellow"
+            });
+        }, 2000);
     }
 }
 
