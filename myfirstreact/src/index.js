4f286eaa917c2c37590c86b7c48d5d22da4b303e
Thanh Pham <thanhpham091790@gmail.com>
2021-02-05 16:01:01 -0700

    Lifecycle of component - Updating - Unmounting

--- myfirstreact/src/index.js
+++ myfirstreact/src/index.js
@@ -5,34 +5,41 @@ class Header extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            favoritecolor: "red"
+            show: true
         };
     }
-    componentDidMount(){
-        setTimeout(()=>{
-            this.setState({
-                favoritecolor: "yellow"
-            });
-        },1000);
+
+    delHeader = () => {
+        this.setState({
+            show: false
+        });
     }
-    getSnapshotBeforeUpdate(prevProps, prevState){
-        document.getElementById("div1").innerHTML = "Before the update, the favorite was " +
-        prevState.favoritecolor;
-    }
-    componentDidUpdate(){
-        document.getElementById("div2"). innerHTML = "The updated favorite is " + this.state.favoritecolor;
-    }
+    
     render() {
+        let myheader;
+        if(this.state.show){
+            myheader = <Child />;
+        }
         return (
             <div>
-                <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-                <div id="div1"></div>
-                <div id="div2"></div>
+                {myheader}
+                <button type="button" onClick={this.delHeader}>Delete Header</button>
             </div>
         );
     }
 
 }
 
-const myHeader = <Header favcol="yellow" />;
+class Child extends React.Component{
+    componentWillUnmount(){
+        alert("The component named Header is about to be unmounted.");
+    }
+    render(){
+        return(
+            <h1>Hello World!</h1>
+        );
+    }
+}
+
+const myHeader = <Header />;
 ReactDOM.render(myHeader, document.getElementById("root"));
