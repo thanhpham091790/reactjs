88262453ea30ce7a15dde0a8ecde4e0501ba82c6
Thanh Pham <thanhpham091790@gmail.com>
2021-02-05 17:17:22 -0700

    React event

---
+++ myfirstreact/src/index_react_events.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Football extends React.Component{
+    shoot = (a,b) => {
+        alert(b.type);
+    }
+    render(){
+        return(
+            <button onClick={this.shoot.bind(this, "Goal")}>Take the shot!</button>
+        );
+    }
+}
+
+ReactDOM.render(<Football />, document.getElementById("root"));
