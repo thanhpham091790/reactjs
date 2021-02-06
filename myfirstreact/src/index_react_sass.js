77b49ff9959a53f9d8b9ca34a53eacfad4f7202e
Thanh Pham <thanhpham091790@gmail.com>
2021-02-05 19:11:04 -0700

    React Sass

---
+++ myfirstreact/src/index_react_sass.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Car from "./App.js"
+import "./mysass.scss"
+
+class MyHeader extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello Style!</h1>
+                <p>Add a little style.</p>
+                <Car />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<MyHeader />, document.getElementById("root"));
