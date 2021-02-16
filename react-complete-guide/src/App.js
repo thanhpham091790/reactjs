c52bd8e0bfd61d756e3ee73470529c37f1feba65
Thanh Pham <thanhpham091790@gmail.com>
2021-02-16 11:55:53 -0700

    Understanding and using state - whenever state changed, the component re-render

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Thanh",age: 32},
+      {name: "Toan",age: 29},
+      {name: "Thuy",age: 26}
+    ]
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person name="Thanh" age="31" />
-        <Person name="Thuy" age="26">My Hobbies: Racing</Person>
-        <Person name="Toan" age="28" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div',{className:'App'}, React.createElement('h1',null, 'My name is Thanh Pham !'));
