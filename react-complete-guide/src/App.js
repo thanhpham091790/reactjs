345e6a44314660455e98203a7aff9b312490df99
Thanh Pham <thanhpham091790@gmail.com>
2021-02-16 11:33:09 -0700

    Functioning component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Person />
       </div>
     );
     // return React.createElement('div',{className:'App'}, React.createElement('h1',null, 'My name is Thanh Pham !'));
