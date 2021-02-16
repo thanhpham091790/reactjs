a3bd8d86e0d5ce694bfaebb66112d8f9353a801a
Thanh Pham <thanhpham091790@gmail.com>
2021-02-16 11:43:43 -0700

    Working with Props for dynamic content

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -7,9 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Thanh" age="31" />
+        <Person name="Thuy" age="26">My Hobbies: Racing</Person>
+        <Person name="Toan" age="28" />
       </div>
     );
     // return React.createElement('div',{className:'App'}, React.createElement('h1',null, 'My name is Thanh Pham !'));
