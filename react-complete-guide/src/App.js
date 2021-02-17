4440f6f71200717c27a067b3c2f6520961f47097
Thanh Pham <thanhpham091790@gmail.com>
2021-02-16 23:07:14 -0700

    What different between stateful and stateless component. Try to make as many as stateless component posible.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,24 +1,19 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = (props) => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       {name: "Thanh",age: 32},
       {name: "Toan",age: 29},
       {name: "Thuy",age: 26}
-    ]
-  });
-
-  const [otherState, setOtherState] = useState({
+    ],
     otherState: "Some other value"
-  });
+  };
 
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
-    setPersonsState({
+  switchNameHandler = () => {
+    this.setState({
       persons: [
         {name: "Thanh Pham",age: 32},
         {name: "Toan Pham",age: 29},
@@ -27,16 +22,18 @@ const App = (props) => {
     });
   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+    // return React.createElement('div',{className:'App'}, React.createElement('h1',null, 'My name is Thanh Pham !'));
+  }
 }
-
 
 export default App;
