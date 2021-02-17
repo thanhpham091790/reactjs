77030faefed4ca1520343a99c89ba7ca3e810d0e
Thanh Pham <thanhpham091790@gmail.com>
2021-02-16 22:59:00 -0700

    Using useState to control state in function-based component

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -4,7 +4,6 @@ const person = (props) => {
     return (
         <div>
             <p>I'm {props.name} and I am {props.age} years old!</p>
-            <p>{props.children}</p>
         </div>
     );
 }
