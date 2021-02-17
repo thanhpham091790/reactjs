258e9f7215ff9ac22e8bc022a0c85ee0a6f1680e
Thanh Pham <thanhpham091790@gmail.com>
2021-02-17 13:48:31 -0700

    List and State. Call an event when click on a list item and update state based on event

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -14,15 +14,11 @@ class App extends Component {
     showPersons: false
   };
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        {name: newName,age: 32},
-        {name: "Toan Pham",age: 29},
-        {name: "Thuy Tran",age: 26}
-      ]
-    });
-  }
+ deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons});
+ }
 
   nameChangeHandler = (event) => {
     this.setState({
@@ -52,8 +48,12 @@ class App extends Component {
     if (this.state.showPersons){
       persons = (
         <div>
-          {this.state.persons.map((person)=>{
-            return <Person name={person.name} age={person.age} />
+          {this.state.persons.map((person, index)=>{
+            return <Person 
+              click={() => this.deletePersonHandler(index)}
+              name={person.name} 
+              age={person.age} 
+              />
           })}
           {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age} 
