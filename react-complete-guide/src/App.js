4f4a038b8890dd3030cd28abfd6c6c42408dbc78
Thanh Pham <thanhpham091790@gmail.com>
2021-02-17 13:28:27 -0700

    Handling dynamic content in Javascript way. We check condition before we return the value

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import person from './Person/Person';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -46,20 +47,25 @@ class App extends Component {
       padding: '8px',
       cursor: 'pointer'
     };
+
+    let persons = null;
+    if (this.state.showPersons){
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age} 
+            click={this.switchNameHandler.bind(this, "Thanh H Pham!")}
+            changed={this.nameChangeHandler} />
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        </div> 
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button style = {style} onClick={this.togglePersonsHandler}>Switch Name</button>
-
-        {this.state.showPersons ? 
-          <div>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age} 
-              click={this.switchNameHandler.bind(this, "Thanh H Pham!")}
-              changed={this.nameChangeHandler} />
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-          </div> 
-        : null}
+        <button style = {style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {persons}
       </div>
     );
   }
