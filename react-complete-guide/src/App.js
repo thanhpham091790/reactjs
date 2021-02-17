3068cc43fd408eaf637618c596e3e8a2d75cb94f
Thanh Pham <thanhpham091790@gmail.com>
2021-02-17 13:39:25 -0700

    Outputing List of Components base on data list from state. Using map() function

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -52,11 +52,14 @@ class App extends Component {
     if (this.state.showPersons){
       persons = (
         <div>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+          {this.state.persons.map((person)=>{
+            return <Person name={person.name} age={person.age} />
+          })}
+          {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age} 
             click={this.switchNameHandler.bind(this, "Thanh H Pham!")}
             changed={this.nameChangeHandler} />
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} /> */}
         </div> 
       );
     }
