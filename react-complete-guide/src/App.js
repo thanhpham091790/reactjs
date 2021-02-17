22bcdc2c3becb019e32e31597f692faae714543a
Thanh Pham <thanhpham091790@gmail.com>
2021-02-16 23:50:09 -0700

    Working with Inline Styles. You can add variable style with an object and assign it for element that you want. This way make your styles don't become global style but it can't take power of CSS. You have to use carmelCase for css and write css inside js file.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -33,10 +33,20 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={() => this.switchNameHandler("Thanh H Pham")}>Switch Name</button>
+        <button 
+          style = {style}
+          onClick={() => this.switchNameHandler("Thanh H Pham")}
+          >Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person 
           name={this.state.persons[1].name} 
