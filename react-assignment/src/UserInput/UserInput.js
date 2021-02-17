c8c698ad8ec0113aae77608b459d0242c8d10525
Thanh Pham <thanhpham091790@gmail.com>
2021-02-17 11:44:02 -0700

    Create two components UserInput and UserOutput. The UserInput hold an input element. The UserOutput  hold two paragraph. And then, output multiple time UserOutput in the App components.

---
+++ react-assignment/src/UserInput/UserInput.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class UserInput extends Component{
+    render(){
+        return(
+            <div className="UserInput">
+                <input type="text" />
+            </div>
+        );
+    }
+}
+
+export default UserInput;
---
+++ react-assignment/src/UserOutput/UserOutput.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class UserOutput extends Component{
+    render(){
+        return(
+            <div className="UserOutput">
+                <p>Paragraph 1</p>
+                <p>Paragraph 2</p>
+            </div>
+        );
+    }
+}
+
+export default UserOutput;
