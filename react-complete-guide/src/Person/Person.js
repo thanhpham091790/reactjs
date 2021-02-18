684550096bf43f84317bb5a437ee564e399afc1b
Thanh Pham <thanhpham091790@gmail.com>
2021-02-18 15:43:35 -0700

    Using radium for media queries. We need to import Radium inside component. Wrap export component with Radium. Cover app component with <styleRoot> tag.

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -3,8 +3,14 @@ import './Person.css';
 import Radium from 'radium';
 
 const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '300px'
+        }
+    };
+
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <input type="text" onChange={props.changed} value={props.name} />
         </div>
