872c656e39a51436dad16c9e3848d9b313736c17
Thanh Pham <thanhpham091790@gmail.com>
2021-02-18 17:13:52 -0700

    Finish styled-component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Person from './Person/Person';
 
-const StyledButton = styled.button`
-    background-color: green;
-    color: white;
-    font: inherit;
-    border: 1px solid blue;
-    padding: 8px;
-    cursor: pointer;
+// const StyledButton = styled.button`
+//     background-color: ${props => props.alt ? 'red': 'green'};
+//     color: white;
+//     font: inherit;
+//     border: 1px solid blue;
+//     padding: 8px;
+//     cursor: pointer;
 
-    &:hover{
-      background-color: lightgreen;
-      color: black;
-    }
-`;
+//     &:hover{
+//       background-color: ${props => props.alt ? 'salmon': 'lightgreen'};
+//       color: black;
+//     }
+// `;
 
 class App extends Component {
   state = {
@@ -57,18 +57,18 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
+    // const style = {
+    //   backgroundColor: 'green',
+    //   color: 'white',
+    //   font: 'inherit',
+    //   border: '1px solid blue',
+    //   padding: '8px',
+    //   cursor: 'pointer',
+    //   ':hover': {
+    //     backgroundColor: 'lightgreen',
+    //     color: 'black'
+    //   }
+    // };
 
     let persons = null;
     if (this.state.showPersons) {
@@ -86,11 +86,11 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: '#ffcccb',
-        color: 'black'
-      }
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // }
     }
 
     let classes = [];
@@ -105,7 +105,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <StyledButton onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+        <button onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
