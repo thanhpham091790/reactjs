ad0f86575e70cd6b12fc9af0aa017a069d4dbcc2
Thanh Pham <thanhpham091790@gmail.com>
2021-02-26 21:09:19 -0700

    Rendering many elements as an array with unique key

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -3,12 +3,10 @@ import classess from './Person.module.css';
 
 class Person extends Component{
     render(){
-        return (
-            <div className={classess.Person}>
-                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
-        );
+        return [
+            <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+            <input key="i2" type="text" onChange={this.props.changed} value={this.props.name} />
+        ];
     }
     
 }
