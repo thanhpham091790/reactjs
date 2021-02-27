46953999da34d79cac709706db0772f65f3fa75e
Thanh Pham <thanhpham091790@gmail.com>
2021-02-26 21:52:24 -0700

    React has a build-in component to wrap another jxs code. It runs exactly Aux.js. It calls Fragment component.

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,14 +1,14 @@
-import React,{Component} from "react"
+import React,{Component, Fragment} from "react"
 import classess from './Person.module.css';
 import Auxi from '../../../hoc/Auxi';
 
 class Person extends Component{
     render(){
         return (
-            <Auxi>
+            <Fragment>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </Auxi>
+            </Fragment>
         );
     }
     
