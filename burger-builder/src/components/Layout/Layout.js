81acb8f3ebcc2f13843ba8a9db0700087736b6e3
Thanh Pham <thanhpham091790@gmail.com>
2021-03-03 22:00:53 -0700

    Starting implementation of the burger builder container

---
+++ burger-builder/src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Auxi from '../../hoc/Auxi';
+import classes from './Layout.module.css';
+
+const layout = (props) => {
+    return (
+        <Auxi>
+            <div>Toolbar, Side Drawer, Backdrop</div>
+            <main className={classes.Content}>
+                {props.children}
+            </main>
+        </Auxi>
+    );
+}
+
+export default layout;
---
+++ burger-builder/src/hoc/Auxi.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const auxi = (props) => {
+    return props.children;
+}
+
+export default auxi;
