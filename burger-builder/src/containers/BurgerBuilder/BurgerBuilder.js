079def498447b364e505ed302f33841318654ae2
Thanh Pham <thanhpham091790@gmail.com>
2021-03-04 16:05:37 -0700

    Adding the BuidControl Component and Outputing multiple BuildControl Components.

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Auxi from '../../hoc/Auxi';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component{
 
@@ -16,10 +17,8 @@ class BurgerBuilder extends Component{
     render(){
         return(
             <Auxi>
-                <div>
-                    <Burger ingredients={this.state.ingredients} />
-                </div>
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
             </Auxi>
         );
     }
