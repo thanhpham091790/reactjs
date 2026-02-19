1dec0a6c2b82d78aa4b092b9c367dfb5f3a63254
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-18 18:02:18 -0700

    React support if statements, but not inside JSX.

--- 01-react-tutorial/my-react-app/src/main.jsx
+++ 01-react-tutorial/my-react-app/src/main.jsx
@@ -4,10 +4,21 @@ import { createRoot } from 'react-dom/client'
 function Car() {
 
   const className = 'greeting';
-  const style = {
-    color: 'red',
-    fontSize: '20px'
-  };
+  const x = 6;
+  let style;
+
+  if (x > 5) {
+    style = {
+      color: 'red',
+      fontSize: '20px'
+    };
+  } else {
+    style = {
+      color: 'blue',
+      fontSize: '40px'
+    }
+  }
+
 
   function onClickHandler() {
     alert('Hello, Thanh Pham!');
