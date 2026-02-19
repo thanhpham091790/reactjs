c6020a84a493feb3bc86afe04c4518fd69412744
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-18 17:05:05 -0700

    With JSX you can write expressions inside curly braces {}

--- 01-react-tutorial/my-react-app/src/main.jsx
+++ 01-react-tutorial/my-react-app/src/main.jsx
@@ -1,6 +1,6 @@
 
 import { createRoot } from 'react-dom/client'
 
-const myElement = <h1>Pham Hong Thanh</h1>
+const myElement = <h1>Pham Hong Thanh. He is {30 + 6} years old.</h1>
 
 createRoot(document.getElementById('root')).render(myElement);
