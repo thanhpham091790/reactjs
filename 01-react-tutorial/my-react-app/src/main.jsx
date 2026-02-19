c3d177a99bfa1cea854c63bf51d86fa8b41d7420
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-18 17:37:44 -0700

    Use attribute className instead of class in JSX.

--- 01-react-tutorial/my-react-app/src/main.jsx
+++ 01-react-tutorial/my-react-app/src/main.jsx
@@ -2,19 +2,10 @@
 import { createRoot } from 'react-dom/client'
 
 function Car() {
-  const myobj = {
-    name: 'Toyota',
-    model: 'Camry',
-    color: 'Silver'
-  }
-
-  function kwtohp(kw) {
-    return kw * 1.36;
-  }
 
   return (
     <>
-      <h2>My car is {myobj.name} {myobj.model}. It has {kwtohp(218)} horsepower.</h2>
+      <h2 className='greeting'>Hello world!</h2>
     </>
   );
 }
