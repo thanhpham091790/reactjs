ed4e04d248d6dc6adfeaf31bf86ecb187835c9f4
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-21 14:51:43 -0700

    React props can be of any data type, including variables, numbers, strings, objects, arrays, and more.

--- 01-react-tutorial/my-react-app/src/Garage.jsx
+++ 01-react-tutorial/my-react-app/src/Garage.jsx
@@ -2,10 +2,11 @@
 import Car from './Car';
 
 function Garage() {
+    let brand = 'Toyota';
     return (
         <>
             <h1>Who live in garage ?</h1>
-            <Car color='salmon' name='Ford' />
+            <Car color='salmon' name={brand} />
             <Car color='green' name='Subaru' />
         </>
     );
