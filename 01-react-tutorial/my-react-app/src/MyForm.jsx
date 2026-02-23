81596efec43abb17a32c034ae69740e3c0608452
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-23 11:59:19 -0700

    We can use the useState Hook to keep track of each input value and provide a single source of truth for entire application.

--- 01-react-tutorial/my-react-app/src/MyForm.jsx
+++ 01-react-tutorial/my-react-app/src/MyForm.jsx
@@ -1,8 +1,23 @@
+
+import { useState } from "react";
+
 function MyForm() {
+    const [name, setName] = useState('');
+
+    function changeHandler(event) {
+        setName(event.target.value);
+    }
+
     return (
         <form>
             <label htmlFor="name">Enter your name:</label>
-            <input type="text" name="name" id="name" />
+            <input
+                type="text"
+                id="name"
+                value={name}
+                onChange={changeHandler}
+            />
+            <p>Current value: {name}</p>
         </form>
     );
 }
