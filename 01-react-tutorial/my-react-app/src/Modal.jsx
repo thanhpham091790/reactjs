4874bd249d2910cffd1c353d127e06240ee04707
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-02-27 00:17:33 -0700

    Portals are particularly useful for: Modals, dialogs, tooltips, floating menus, notifications.

--- 01-react-tutorial/my-react-app/src/Modal.jsx
+++ 01-react-tutorial/my-react-app/src/Modal.jsx
@@ -6,9 +6,30 @@ function Modal(props) {
 
     return (
         isOpen ? createPortal(
-            <div id='modal'>
-                <div>{children}</div>
-                <button type='button' onClick={onClose}>Close</button>
+            <div
+                id='modal'
+                style={{
+                    position: 'fixed',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <div
+                    style={{
+                        background: 'white',
+                        padding: '20px',
+                        borderRadius: '8px'
+                    }}
+                >
+                    {children}
+                    <button type='button' onClick={onClose}>Close</button>
+                </div>
             </div>,
             document.body
         ) : null
