1df8c27605252d07d8952eb53df88cebc0716823
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-03-01 02:02:24 -0700

    Sass has many built-in modules dthat you can use to manipulate colors, math, strings, etc.

--- 01-react-tutorial/my-react-app/src/App.jsx
+++ 01-react-tutorial/my-react-app/src/App.jsx
@@ -10,6 +10,9 @@ function App() {
     <div>
       <Greeting name='Toan' />
       <GreetingWithBorder name='Thanh' />
+      <h1>My Header 1</h1>
+      <h2>My Header 2</h2>
+      <h3>My Header 3</h3>
     </div>
   );
 }
