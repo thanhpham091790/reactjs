1bdd2263f1da4a31ee75bc0bd3252385840928f7
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-03-01 01:09:34 -0700

    Focusing input elements by using useRef hook.

--- 01-react-tutorial/my-react-app/src/App.jsx
+++ 01-react-tutorial/my-react-app/src/App.jsx
@@ -1,47 +1,16 @@
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";
-import Products from './Products';
-import BikeProducts from './BikeProducts';
-import CarProducts from './CarProducts';
-import Info from './Info';
-
-
+import { useRef } from "react";
 function App() {
+  const inputRef = useRef();
 
-  const navLinkStyles = ({ isActive }) => {
-    return {
-      color: isActive ? '#007bff' : '#333',
-      textDecoration: isActive ? 'none' : 'underline',
-      fontWeight: isActive ? 'bold' : 'normal',
-      padding: '5px 10px'
-    };
-  };
+  function focusInput() {
+    inputRef.current.focus();
+  }
 
   return (
-    <BrowserRouter>
-      {/* Navigation */}
-      <nav>
-        <NavLink style={navLinkStyles} to='/'>Home</NavLink> |{" "}
-        <NavLink style={navLinkStyles} to='/about'>About</NavLink> |{" "}
-        <NavLink style={navLinkStyles} to='/contact'>Contact</NavLink> |{" "}
-        <NavLink style={navLinkStyles} to='./products'>Products</NavLink>
-        <NavLink style={navLinkStyles} to='./customer/Thanh'>Thanh</NavLink>
-        <NavLink style={navLinkStyles} to='./customer/Toan'>Toan</NavLink>
-      </nav>
-      {/* Routes */}
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/products' element={<Products />}>
-          <Route path='car' element={<CarProducts />} />
-          <Route path='bike' element={<BikeProducts />} />
-        </Route>
-        <Route path='/customer/:firstname' element={<Info />} />
-      </Routes>
-    </BrowserRouter >
+    <div>
+      <input type="text" ref={inputRef} placeholder="Type here..." />
+      <button type="button" onClick={focusInput}>Focus Input</button>
+    </div>
   );
 }
 
