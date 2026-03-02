1ec7999d445be74b1bca1cdbaa5c02e8339b0950
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-03-02 01:39:16 -0700

    Here is an example of a useEffect hook that is dependent on a variable. If the count variable updates, the effect will run again.

--- 01-react-tutorial/my-react-app/src/App.jsx
+++ 01-react-tutorial/my-react-app/src/App.jsx
@@ -4,16 +4,17 @@ import { useState, useEffect } from "react";
 function App() {
 
   const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
 
   useEffect(() => {
-    setTimeout(() => {
-      setCount(prevCount => prevCount + 1);
-    }, 1000);
-  }, []);
+    setCalculation(count * 2);
+  }, [count]);
 
   return (
     <>
-      <h1>I've rendered {count} times!</h1>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount((c) => c + 1)}>+</button>
+      <p>Calculation: {calculation}</p>
     </>
   );
 }
