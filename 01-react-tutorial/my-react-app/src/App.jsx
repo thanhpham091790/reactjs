fc09d430f59c982e841475550ab0e4c567e9e9ff
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-03-05 11:40:30 -0700

    The useMemo hook accepts a second parameter to declare dependencies. The expensive function will only run when its dependencies have changed.

--- 01-react-tutorial/my-react-app/src/App.jsx
+++ 01-react-tutorial/my-react-app/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 
 function App() {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
+  const result = useMemo(() => expensiveCalculation(count), [count])
 
   const clickHandler = () => {
     setCount(count + 1);
@@ -11,14 +12,6 @@ function App() {
 
   const addTodoHandler = () => {
     setTodos((prevTodos) => [...prevTodos, 'New Todo']);
-  }
-
-  const expensiveCalculation = (number) => {
-    console.log('Calculating...');
-    for (let i = 0; i < 1000000000; i++) {
-      number += 1;
-    }
-    return number;
   }
 
   return (
@@ -35,11 +28,19 @@ function App() {
         <p>Count: {count}</p>
         <button type='button' onClick={clickHandler}>+</button>
         <h2>Expensive Calculation</h2>
-        <p>{expensiveCalculation(count)}</p>
+        <p>{result}</p>
         <p>Note that this example executes the expensive function also when you click on the Add Todo button.</p>
       </div>
     </div>
   );
+}
+
+const expensiveCalculation = (number) => {
+  console.log('Calculating...');
+  for (let i = 0; i < 1000000000; i++) {
+    number += 1;
+  }
+  return number;
 }
 
 export default App;
