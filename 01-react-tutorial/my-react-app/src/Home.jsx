15c67494c14e99dcc457bc678e72c1bc6720a6d8
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-03-05 22:22:27 -0700

    The logic behind he fetch may be needed in other components as well.

--- 01-react-tutorial/my-react-app/src/Home.jsx
+++ 01-react-tutorial/my-react-app/src/Home.jsx
@@ -1,13 +1,8 @@
-import { useState, useEffect } from "react";
+
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos')
-            .then(res => res.json())
-            .then(data => setData(data));
-    }, []);
+    const [data] = useFetch('https://jsonplaceholder.typicode.com/todos');
 
     console.log(data);
 
