cc4399abe0a2a70c377841b8904429a86bd1cda6
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-03-12 16:55:03 -0600

    Add favicon icon and all button.

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,3 +1,15 @@
 export default function Square() {
-  return <button className="square">X</button>
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
 }
