232d2955abfe5587d0871a68f98fb4bd1c5851e9
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-03-12 17:35:44 -0600

    Declare a function called handClick  inside of the Square component. The add onClick to the props of the button.

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,7 +1,15 @@
 export default function Board() {
 
   function Square({ value }) {
-    return <button className="square">{value}</button>
+    function handleClick() {
+      console.log('Clicked!');
+    }
+    return (
+      <button
+        className="square"
+        onClick={handleClick}
+      >{value}</button>
+    )
   }
 
   return (
