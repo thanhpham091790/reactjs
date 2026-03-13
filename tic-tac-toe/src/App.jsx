d79611f4228742026a7dd57090d73f35441644e0
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-03-12 18:25:27 -0600

    Store the game state  in the parent Board component instead of in each Square. The Board component can tell each Square what to display by passing a prop.

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 export default function Board() {
+  const [square, setSquare] = useState(Array(9).fill(null));
 
 
+  function Square({ value }) {
 
-  function Square() {
-    const [value, setValue] = useState(null);
 
     function handleClick() {
       setValue('X');
@@ -21,21 +21,21 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[0]} />
+        <Square value={square[1]} />
+        <Square value={square[2]} />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[3]} />
+        <Square value={square[4]} />
+        <Square value={square[5]} />
       </div>
 
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={square[6]} />
+        <Square value={square[7]} />
+        <Square value={square[8]} />
       </div>
 
     </>
