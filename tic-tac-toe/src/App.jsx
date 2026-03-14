7bacb7d452dd555ed5f548ac4307216fe72e90dd
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-03-14 14:47:39 -0600

    For current move only, show "You are at move #..." instead of a button.

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -21,13 +21,17 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = 'Go to move #' + move;
+      if (move === currentMove) {
+        description = 'You are at move #' + move;
+      } else {
+        description = 'Go to move #' + move;
+      }
     } else {
       description = 'Go to game start';
     }
     return (
       <li key={move}>
-        <button onClick={() => { jumpTo(move) }}>{description}</button>
+        {move === currentMove ? description : <button onClick={() => { jumpTo(move) }}>{description}</button>}
       </li>
     );
   });
