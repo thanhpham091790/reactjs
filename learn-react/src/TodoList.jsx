5ac92d8736a621b0597b61fcba28db1cc0442fff
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-04-25 20:34:55 -0600

    Javascript in JSX with curly braces.

--- learn-react/src/TodoList.jsx
+++ learn-react/src/TodoList.jsx
@@ -1,16 +1,24 @@
 export default function TodoList() {
+  const person = {
+    name: "Hedy Lamarr",
+    theme: {
+      backgroundColor: "black",
+      color: "pink",
+    },
+  };
   return (
-    <>
-      <h1>Hedy Lamarr's Todos</h1>
+    <div className="todo-list" style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
       <img
+        className="avatar"
         src="https://react.dev/images/docs/scientists/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
+        alt={person.name}
       />
       <ul>
         <li>Invent new traffic lights</li>
         <li>Rehearse a movie scene</li>
         <li>Improve spectrum technology</li>
       </ul>
-    </>
+    </div>
   );
 }
