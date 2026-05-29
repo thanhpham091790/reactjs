b88b4db099134a7db134300a3b4a6d8892043477
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-05-29 01:59:08 -0600

    Exporting and importing a component.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,19 +1,5 @@
-function Profile() {
-  return (
-    <img
-      src="https://react.dev/images/docs/scientists/MK3eW3Am.jpg"
-      alt="Katherine Johnson"
-    />
-  );
-}
+import Gallery from "./Gallery";
 
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  );
+export default function App() {
+  return <Gallery />;
 }
