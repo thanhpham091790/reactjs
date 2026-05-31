299ddc3d7ac62f467bf42eaa07d84001b81fe656
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-05-30 20:32:39 -0600

    Converting HTML to JSX

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,6 +1,17 @@
-import Gallery from "./Gallery";
-import { Profile } from "./Gallery";
-
-export default function App() {
-  return <Profile />;
+export default function TodoList() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://react.dev/images/docs/scientists/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        class="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
 }
