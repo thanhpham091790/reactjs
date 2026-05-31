647d99eb2d73a4f9acfa61ad9802ed1a03c8f16c
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-05-30 21:08:20 -0600

    Using double curly braces for objects in JSX.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,20 +1,25 @@
-const today = new Date();
-
-function formatDate(date) {
-  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
-}
+const person = {
+  name: "Thanh Pham",
+  theme: {
+    backgroundColor: "black",
+    color: "pink",
+  },
+};
 
 export default function TodoList() {
   return (
-    <ul
-      style={{
-        backgroundColor: "black",
-        color: "pink",
-      }}
-    >
-      <li>Improve the videophone</li>
-      <li>Prepare aeronautics lectures</li>
-      <li>Work on the alcohol-fuelled engine</li>
-    </ul>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://react.dev/images/docs/scientists/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
   );
 }
