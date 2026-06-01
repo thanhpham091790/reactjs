fa9598925aa852cd645eb14480d1522715201da7
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-06-01 12:00:24 -0600

    This works not only for text, but for arbitrary JSX too.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,6 +1,6 @@
 function Item({ name, isPacked }) {
   let itemContent = name;
-  if (isPacked) itemContent = name + " ✅";
+  if (isPacked) itemContent = <del>{name + " ✅"}</del>;
   return <li className="item">{itemContent}</li>;
 }
 
