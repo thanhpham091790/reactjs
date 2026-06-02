21b9e141dbb4fb62f11755251fd516a698a61778
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-06-02 00:30:47 -0600

    Rendering data from arrays.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,18 +1,14 @@
-function Item({ name, isPacked }) {
-  let itemContent = name;
-  if (isPacked) itemContent = <del>{name + " ✅"}</del>;
-  return <li className="item">{itemContent}</li>;
-}
+const people = [
+  "Creola Katherine Johnson: mathematician",
+  "Mario José Molina-Pasquel Henríquez: chemist",
+  "Mohammad Abdus Salam: physicist",
+  "Percy Lavon Julian: chemist",
+  "Subrahmanyan Chandrasekhar: astrophysicist",
+];
 
-export default function PackingList() {
-  return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item isPacked={true} name="Space suit" />
-        <Item isPacked={true} name="Helmet with a golden leaf" />
-        <Item isPacked={false} name="Photo of Tam" />
-      </ul>
-    </section>
-  );
+export default function List() {
+  const listItems = people.map((person, index) => (
+    <li key={index}>{person}</li>
+  ));
+  return <ul>{listItems}</ul>;
 }
