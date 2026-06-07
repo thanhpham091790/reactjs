071fd43cefe8e6fa92e5abf31465f27560d27129
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-06-07 00:55:28 -0600

    Responding to events.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,16 +1,21 @@
-import { people } from "./data";
-import { getImageUrl } from "./utils";
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
+}
 
-export default function List() {
-  const chemists = people.filter((person) => person.profession === "chemist");
-  const listItems = chemists.map((person) => (
-    <li key={person.id}>
-      <img src={getImageUrl(person)} alt={person.name} />
-      <p>
-        <b>{person.name}:</b>
-        {" " + person.profession + " "} known for {person.accomplishment}
-      </p>
-    </li>
-  ));
-  return <ul>{listItems}</ul>;
+function Toolbar({ onPlayMovie, onUploadImage }) {
+  return (
+    <div>
+      <Button onClick={onPlayMovie}>Play Movie</Button>
+      <Button onClick={onUploadImage}>Upload Image</Button>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Toolbar
+      onPlayMovie={() => alert("Playing")}
+      onUploadImage={() => alert("Uploading")}
+    />
+  );
 }
