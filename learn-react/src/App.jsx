5de472329cb7788a38e789061e6382415e7b566e
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-06-07 01:55:25 -0600

    State: a component memory.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,21 +1,43 @@
-function Button({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>;
-}
+import { sculptureList } from "./data.js";
+import { useState } from "react";
 
-function Toolbar({ onPlayMovie, onUploadImage }) {
-  return (
-    <div>
-      <Button onClick={onPlayMovie}>Play Movie</Button>
-      <Button onClick={onUploadImage}>Upload Image</Button>
-    </div>
-  );
-}
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [show, setShow] = useState(false);
 
-export default function App() {
+  let styles = show ? { display: "block" } : { display: "none" };
+
+  function clickNextHandler() {
+    let hasNext = false;
+    if (index < sculptureList.length - 1) {
+      hasNext = true;
+    }
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function clickShowHandler() {
+    setShow(!show);
+  }
+
   return (
-    <Toolbar
-      onPlayMovie={() => alert("Playing")}
-      onUploadImage={() => alert("Uploading")}
-    />
+    <>
+      <button onClick={clickNextHandler}>Next</button>
+      <h1>
+        <i>{sculptureList[index].name}</i> by {sculptureList[index].artist}
+      </h1>
+      <p>
+        ({index + 1} of {sculptureList.length})
+      </p>
+      <button onClick={clickShowHandler}>
+        {show ? "Hide" : "Show"} details
+      </button>
+      <p style={styles}>{sculptureList[index].description}</p>
+      <br />
+      <img src={sculptureList[index].url} alt={sculptureList[index].alt} />
+    </>
   );
 }
