c7c011975274e2e19737e3acfdf7b055f4b47570
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-06-13 17:47:51 -0600

    Setting state triggers renders

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,43 +1,24 @@
-import { sculptureList } from "./data.js";
 import { useState } from "react";
+export default function Form() {
+  const [message, setMessage] = useState("Hi! Write something.");
+  const [isSent, setIsSent] = useState(false);
 
-export default function Gallery() {
-  const [index, setIndex] = useState(0);
-  const [show, setShow] = useState(false);
-
-  let styles = show ? { display: "block" } : { display: "none" };
-
-  function clickNextHandler() {
-    let hasNext = false;
-    if (index < sculptureList.length - 1) {
-      hasNext = true;
-    }
-    if (hasNext) {
-      setIndex(index + 1);
-    } else {
-      setIndex(0);
-    }
+  function messageChangeHandler(e) {
+    setMessage(e.target.value);
   }
 
-  function clickShowHandler() {
-    setShow(!show);
+  function submitHandler(e) {
+    e.preventDefault();
+    setIsSent(true);
   }
+
 
   return (
-    <>
-      <button onClick={clickNextHandler}>Next</button>
-      <h1>
-        <i>{sculptureList[index].name}</i> by {sculptureList[index].artist}
-      </h1>
-      <p>
-        ({index + 1} of {sculptureList.length})
-      </p>
-      <button onClick={clickShowHandler}>
-        {show ? "Hide" : "Show"} details
-      </button>
-      <p style={styles}>{sculptureList[index].description}</p>
+    isSent ? <h1>Thank you for send message.</h1> :
+    <form onSubmit={submitHandler}>
+      <textarea onChange={messageChangeHandler}>{message}</textarea>
       <br />
-      <img src={sculptureList[index].url} alt={sculptureList[index].alt} />
-    </>
+      <input type="submit" value="Send" />
+    </form>
   );
 }
