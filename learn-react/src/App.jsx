5091823c93f7c29ace780726a934602e7350da38
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-06-13 18:56:13 -0600

    Rendering takes a snapshot in time.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,24 +1,16 @@
 import { useState } from "react";
-export default function Form() {
-  const [message, setMessage] = useState("Hi! Write something.");
-  const [isSent, setIsSent] = useState(false);
 
-  function messageChangeHandler(e) {
-    setMessage(e.target.value);
+export default function Counter() {
+  const [number, setNumber] = useState(0);
+  function numberChangeHandler() {
+    setNumber(number + 1);
+    setNumber(number + 1);
+    setNumber(number + 1);
   }
-
-  function submitHandler(e) {
-    e.preventDefault();
-    setIsSent(true);
-  }
-
-
   return (
-    isSent ? <h1>Thank you for send message.</h1> :
-    <form onSubmit={submitHandler}>
-      <textarea onChange={messageChangeHandler}>{message}</textarea>
-      <br />
-      <input type="submit" value="Send" />
-    </form>
+    <>
+      <h1>{number}</h1>
+      <button onClick={numberChangeHandler}>+3</button>
+    </>
   );
 }
