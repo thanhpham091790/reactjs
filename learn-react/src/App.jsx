72d5b9dd74c44f6969ad9efebac46999dfe2a256
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-06-13 20:59:41 -0600

    React keeps the state values fixed within one render event handler.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,17 +1,36 @@
 import { useState } from "react";
 
-export default function Counter() {
-  const [number, setNumber] = useState(0);
-  function numberChangeHandler() {
-    setNumber(number + 5);
+export default function Form() {
+  const [to, setTo] = useState("Alice");
+  const [message, setMessage] = useState("Hello");
+
+  function submitHandler(e) {
+    e.preventDefault();
     setTimeout(() => {
-      alert(number);
-    }, 3000);
+      alert(`You said ${message} to ${to}`);
+    }, 5000);
   }
+
   return (
-    <>
-      <h1>{number}</h1>
-      <button onClick={numberChangeHandler}>+5</button>
-    </>
+    <form onSubmit={submitHandler}>
+      <div>
+        <label htmlFor="person">To: </label>
+        <select value={to} id="person" onChange={(e) => setTo(e.target.value)}>
+          <option value="Alice">Alice</option>
+          <option value="Bob">Bob</option>
+        </select>
+      </div>
+
+      <div>
+        <textarea
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          placeholder="Message"
+        />
+      </div>
+      <div>
+        <input type="submit" value="Send" />
+      </div>
+    </form>
   );
 }
