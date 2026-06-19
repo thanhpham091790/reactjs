f1229af84e1d2a9575840272632be4b4e7d2698a
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-06-18 22:56:21 -0600

    Treat state as read-only.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,33 +1,42 @@
 import { useState } from "react";
 
-export default function RequestTracker() {
-  const [pending, setPending] = useState(0);
-  const [completed, setCompleted] = useState(0);
+export default function MovingDot() {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0,
+  });
 
-  async function clickHandler() {
-    setPending((prevPending) => prevPending + 1);
-    await delay(3000);
-    setPending((prevPending) => prevPending - 1);
-    setCompleted((prevCompleted) => prevCompleted + 1);
-  }
-
-  function delay(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
+  function pointerMoveHander(e) {
+    setPosition({
+      x: e.clientX,
+      y: e.clientY,
     });
   }
 
   return (
-    <>
-      <p>
-        <b>Pending: {pending}</b>
-      </p>
-      <p>
-        <b>Completed: {completed}</b>
-      </p>
-      <p>
-        <button onClick={clickHandler}>Buy</button>
-      </p>
-    </>
+    <div
+      style={{
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
+        border: "1px solid grey",
+        borderRadius: "10px",
+        margin: "0 auto",
+      }}
+      onPointerMove={pointerMoveHander}
+    >
+      <div
+        style={{
+          position: "absolute",
+          width: "20px",
+          height: "20px",
+          borderRadius: "50%",
+          backgroundColor: "red",
+          left: "-10px",
+          top: "-10px",
+          transform: `translate(${position.x}px,${position.y}px)`,
+        }}
+      ></div>
+    </div>
   );
 }
