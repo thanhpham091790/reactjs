48c374b010bf61e44d7c58cc45f1c06444b6d29c
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-07-18 12:19:03 -0600

    State: a components memory.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,23 +1,36 @@
-import { Children } from "react";
+import { sculptureList } from "./data.js";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <Toolbar
-      onPlayMovie={() => alert("Playing!")}
-      onUploadImage={() => alert("Uploading!")}
-    />
-  );
-}
+  const [show, setShow] = useState(false);
+  const [index, setIndex] = useState(0);
 
-function Toolbar({ onPlayMovie, onUploadImage }) {
-  return (
-    <div>
-      <Button onClick={onPlayMovie}>Play Movie</Button>
-      <Button onClick={onUploadImage}>Upload Image</Button>
-    </div>
-  );
-}
+  function handleShow() {
+    setShow((prevShow) => !prevShow);
+  }
 
-function Button({ onClick, children }) {
-  return <button onClick={onClick}>{children}</button>;
+  function handleNext() {
+    if (index < sculptureList.length - 1) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  return (
+    <>
+      <button onClick={handleNext}>Next</button>
+      <h2>
+        <i>{sculptureList[index].name}</i> by {sculptureList[index].artist}
+      </h2>
+      <p>
+        ({index + 1} of {sculptureList.length})
+      </p>
+      <button onClick={handleShow}>{show ? "Hide" : "Show"} details</button>
+      {show && <p>{sculptureList[index].description}</p>}
+      <p>
+        <img src={sculptureList[index].url} alt={sculptureList[index].alt} />
+      </p>
+    </>
+  );
 }
