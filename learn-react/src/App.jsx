8ce3d09cf1b2d493a8c5398bb01906128534d677
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-07-20 15:49:17 -0600

    Updating objects in state.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,28 +1,89 @@
 import { useState } from "react";
 
 export default function App() {
-  const [score, setScore] = useState(0);
+  const [person, setPerson] = useState({
+    name: "Niki de Saint Phalle",
+    artwork: {
+      title: "Blue Nana",
+      city: "Hamburg",
+      image: "https://react.dev/images/docs/scientists/Sd1AgUOm.jpg",
+    },
+  });
 
-  function handleIncrement() {
-    setScore((prevScore) => prevScore + 1);
+  function handleNameChange(event) {
+    setPerson({
+      ...person,
+      name: event.target.value,
+    });
   }
+
+  function handleTitleChange(event) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        title: event.target.value,
+      },
+    });
+  }
+
+  function handleCityChange(event) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        city: event.target.value,
+      },
+    });
+  }
+
+  function handleImageChange(event) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: event.target.value,
+      },
+    });
+  }
+
   return (
     <>
-      <div>
-        <button onClick={() => handleIncrement()}>+1</button>
-        <button
-          onClick={() => {
-            handleIncrement();
-            handleIncrement();
-            handleIncrement();
-          }}
-        >
-          +3
-        </button>
-      </div>
-      <div>
-        <h2>Score: {score}</h2>
-      </div>
+      <p>
+        Name:{" "}
+        <input type="text" value={person.name} onChange={handleNameChange} />
+      </p>
+      <p>
+        Title:{" "}
+        <input
+          type="text"
+          value={person.artwork.title}
+          onChange={handleTitleChange}
+        />
+      </p>
+      <p>
+        City:{" "}
+        <input
+          type="text"
+          value={person.artwork.city}
+          onChange={handleCityChange}
+        />
+      </p>
+      <p>
+        Image:{" "}
+        <input
+          type="text"
+          value={person.artwork.image}
+          onChange={handleImageChange}
+        />
+      </p>
+      <p>
+        <i>{person.artwork.title}</i> by {person.name} (located in{" "}
+        {person.artwork.city})
+      </p>
+      <p>
+        <img src={person.artwork.image} alt={person.artwork.title} />
+      </p>
     </>
   );
 }
