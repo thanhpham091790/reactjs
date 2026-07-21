37b7d7790111aaf642426c1b8ff258d3988ddcc0
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-07-21 02:03:59 -0600

    Preventing default behavior.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,28 +1,12 @@
-import "./App.css";
-
 export default function App() {
   return (
-    <div
-      className="Toolbar"
-      onClick={() => alert("You clicked on the toolbar!")}
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        alert("Submitting!");
+      }}
     >
-      <button
-        onClick={(e) => {
-          e.stopPropagation();
-          alert("Playing!");
-        }}
-      >
-        Play Movie
-      </button>{" "}
-      {"  "}
-      <button
-        onClick={(e) => {
-          e.stopPropagation();
-          alert("Uploading!");
-        }}
-      >
-        Upload Image
-      </button>
-    </div>
+      <input type="text" /> <button>Send</button>
+    </form>
   );
 }
