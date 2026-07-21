5538bf25f145f6f8c6ce409809971bd9172cec14
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-07-21 01:02:48 -0600

    Adding event handlers.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,42 +1,7 @@
-import { useState } from "react";
-
-const initialList = [
-  { id: 0, title: "Big Bellies", seen: false },
-  { id: 1, title: "Lunar Landscape", seen: false },
-  { id: 2, title: "Terracotta Army", seen: true },
-];
-
 export default function App() {
-  const [list, setList] = useState(initialList);
-
-  function handleCheckboxChange(id, nextSeen) {
-    setList(
-      list.map((artwork) => {
-        if (artwork.id === id) {
-          return { ...artwork, seen: nextSeen };
-        } else {
-          return artwork;
-        }
-      }),
-    );
+  function handleClick() {
+    alert("You clicked me!");
   }
 
-  return (
-    <>
-      <h1>Art Bucket List</h1>
-      <h2>My list of art to see:</h2>
-      <ul>
-        {list.map((art) => (
-          <li key={art.id}>
-            <input
-              type="checkbox"
-              checked={art.seen}
-              onChange={(e) => handleCheckboxChange(art.id, e.target.checked)}
-            />{" "}
-            {art.title}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  return <button onClick={handleClick}>I don't do anything</button>;
 }
