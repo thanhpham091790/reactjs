2f340cd8920a2ed7ccb9ba4bb8c84f2c51a1b4c7
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-07-21 01:59:26 -0600

    Stopping propagation.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -6,8 +6,23 @@ export default function App() {
       className="Toolbar"
       onClick={() => alert("You clicked on the toolbar!")}
     >
-      <button onClick={() => alert("Playing!")}>Play Movie</button> {"  "}
-      <button onClick={() => alert("Uploading!")}>Upload Image</button>
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          alert("Playing!");
+        }}
+      >
+        Play Movie
+      </button>{" "}
+      {"  "}
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          alert("Uploading!");
+        }}
+      >
+        Upload Image
+      </button>
     </div>
   );
 }
