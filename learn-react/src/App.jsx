161fe2630737cdc888457f67bff0dad1aaf03983
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-07-21 01:27:23 -0600

    Passing event handlers as props.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,16 +1,23 @@
+function Button({ onClick, children }) {
+  return <button onClick={onClick}>{children}</button>;
+}
+
+function PlayButton({ name }) {
+  function handleClick() {
+    alert(`Playing ${name}`);
+  }
+  return <Button onClick={handleClick}>{`Playing "${name}"`}</Button>;
+}
+
+function UploadButton() {
+  return <Button onClick={() => alert("Uploading!")}>Upload Image</Button>;
+}
+
 export default function App() {
   return (
     <>
-      <AlertButton message="Playing!">Play Movie</AlertButton>{" "}
-      <AlertButton message="Uploading!">Upload Image</AlertButton>
+      <PlayButton name="Kiki's Delivery Service" />
+      <UploadButton />
     </>
   );
-}
-
-function AlertButton({ message, children }) {
-  function handleClick(mes) {
-    alert(mes);
-  }
-
-  return <button onClick={() => handleClick(message)}>{children}</button>;
 }
