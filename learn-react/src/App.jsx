313c70c4f5c22c747263ef9952c71dfa1a7621f4
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-07-22 22:11:04 -0600

    When a regular variable is not enough.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,12 +1,25 @@
+import { sculptureList } from "./data";
+
 export default function App() {
+  let index = 0;
+  let sculpture = sculptureList[index];
+
+  function handleClick() {
+    index = index + 1;
+  }
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        alert("Submitting!");
-      }}
-    >
-      <input type="text" /> <button>Send</button>
-    </form>
+    <>
+      <button onClick={handleClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <p>{sculpture.description}</p>
+    </>
   );
 }
