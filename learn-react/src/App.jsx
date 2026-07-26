051be7ee4a15b038b20e873a477d3870eb119841
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-07-26 08:46:12 -0600

    A state variable value never changes within a render.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -3,14 +3,15 @@ import { useState } from "react";
 export default function Counter() {
   const [number, setNumber] = useState(0);
   function handleClick() {
-    setNumber(number + 1);
-    setNumber(number + 1);
-    setNumber(number + 1);
+    setNumber(number + 5);
+    setTimeout(() => {
+      alert(number);
+    }, 3000);
   }
   return (
     <>
       <h1>{number}</h1>
-      <button onClick={handleClick}>+3</button>
+      <button onClick={handleClick}>+5</button>
     </>
   );
 }
