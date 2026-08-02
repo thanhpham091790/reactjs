e3866cd601a7311ccf1f9da50022a49fb4d98238
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-08-02 02:51:56 -0600

    It does not different whether you put the alert before or after the setWalk call.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,41 +1,17 @@
 import { useState } from "react";
 
-export default function Form() {
-  const [to, setTo] = useState("Alice");
-  const [message, setMessage] = useState("Hi, how are you ?");
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTimeout(() => {
-      alert(`Hello, ${to}`);
-    }, 4000);
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
+  function handleClick() {
+    alert(`${walk ? "Walk. Stop is next!" : "Stop. Walk is next!"}`);
+    setWalk(!walk);
   }
-  function handleChangeMessage(e) {
-    setMessage(e.target.value);
-  }
-  function handleChangeTo(e) {
-    setTo(e.target.value);
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <p>
-        <label>To:</label>{" "}
-        <select value={to} onChange={handleChangeTo}>
-          <option value="Alice">Alice</option>
-          <option value="Bob">Bob</option>
-        </select>
-      </p>
-      <p>
-        <textarea
-          placeholder="Message"
-          value={message}
-          onChange={handleChangeMessage}
-        />
-      </p>
-      <p>
-        <button>Send</button>
-      </p>
-    </form>
+    <>
+      <button onClick={handleClick}>Change to {walk ? "Stop" : "Walk"}</button>
+      <h1 style={{ color: walk ? "darkgreen" : "darkred" }}>
+        {walk ? "Walk" : "Stop"}
+      </h1>
+    </>
   );
 }
