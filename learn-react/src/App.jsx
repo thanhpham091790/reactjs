c65a1b770ba33204701b846811eb9c1635da9acd
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-08-06 12:00:42 -0600

    Fix a request counter

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from "react";
 
-export default function Counter() {
-  const [number, setNumber] = useState(0);
-  function handleClick() {
-    setNumber(number + 5);
-    setNumber((prevNumber) => prevNumber + 1);
-    setNumber(37);
+export default function RequestTracker() {
+  const [pending, setPending] = useState(0);
+  const [completed, setCompleted] = useState(0);
+
+  async function handleClick() {
+    setPending((prevPending) => prevPending + 1);
+    await delay(3000);
+    setPending((prevPending) => prevPending - 1);
+    setCompleted((prevCompleted) => prevCompleted + 1);
   }
+
+  function delay(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
   return (
     <>
-      <h1>{number}</h1>
-      <button onClick={handleClick}>Increase the number</button>
+      <p>
+        <b>Pending: {pending}</b>
+      </p>
+      <p>
+        <b>Completed: {completed}</b>
+      </p>
+      <p>
+        <button onClick={handleClick}>Buy</button>
+      </p>
     </>
   );
 }
