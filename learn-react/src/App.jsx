4181daf65d78aa793afb5e9f1dd471ca3f8bb800
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-08-09 01:14:27 -0600

    Adding to an array.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,93 +1,33 @@
 import { useState } from "react";
 
-export default function Form() {
+let nextId = 0;
+
+export default function List() {
   // States
-  const [person, setPerson] = useState({
-    name: "Niki de Saint Phalle",
-    artwork: {
-      title: "Blue Nana",
-      city: "Hamburg",
-      image: "https://react.dev/images/docs/scientists/Sd1AgUOm.jpg",
-    },
-  });
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
 
   // Handlers
   function handleNameChange(e) {
-    setPerson({
-      ...person,
-      name: e.target.value,
-    });
+    setName(e.target.value);
   }
 
-  function handleTitleChange(e) {
-    setPerson({
-      ...person,
-      artwork: {
-        ...person.artwork,
-        title: e.target.value,
-      },
-    });
-  }
-
-  function handleCityChange(e) {
-    setPerson({
-      ...person,
-      artwork: {
-        ...person.artwork,
-        city: e.target.value,
-      },
-    });
-  }
-
-  function handleImageChange(e) {
-    setPerson({
-      ...person,
-      artwork: {
-        ...person.artwork,
-        image: e.target.value,
-      },
-    });
+  function handleButtonClick(e) {
+    setArtists([...artists, { id: nextId++, name: name }]);
   }
 
   return (
-    <form>
-      <label>Name: </label>{" "}
-      <input
-        type="text"
-        name="name"
-        value={person.name}
-        onChange={handleNameChange}
-      />
-      <br />
-      <label>Title: </label>{" "}
-      <input
-        type="text"
-        name="title"
-        value={person.artwork.title}
-        onChange={handleTitleChange}
-      />
-      <br />
-      <label>City: </label>{" "}
-      <input
-        type="text"
-        name="city"
-        value={person.artwork.city}
-        onChange={handleCityChange}
-      />
-      <br />
-      <label>Image: </label>{" "}
-      <input
-        type="text"
-        name="image"
-        value={person.artwork.image}
-        onChange={handleImageChange}
-      />
-      <br />
-      <p>
-        {person.artwork.title} by {person.name} (located in{" "}
-        {person.artwork.city})
-      </p>
-      <img src={person.artwork.image} alt={person.artwork.title} />
-    </form>
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <input type="text" name="name" onChange={handleNameChange} />{" "}
+      <button onClick={handleButtonClick}>Add</button>
+      <div>
+        <ul>
+          {artists.map((artist) => {
+            return <li key={artist.id}>{artist.name}</li>;
+          })}
+        </ul>
+      </div>
+    </>
   );
 }
