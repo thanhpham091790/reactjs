0ad3dfd55d51e48cbdda294879a891747fa0378c
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-08-09 01:22:07 -0600

    Removing from an array.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from "react";
 
-let nextId = 0;
+let initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
 
 export default function List() {
   // States
-  const [name, setName] = useState("");
-  const [artists, setArtists] = useState([]);
+  const [artists, setArtists] = useState(initialArtists);
 
   // Handlers
-  function handleNameChange(e) {
-    setName(e.target.value);
-  }
-
-  function handleButtonClick(e) {
-    setArtists([...artists, { id: nextId++, name: name }]);
+  function handleDeleteButtonClick(id) {
+    let updateArtists = artists.filter((artist) => {
+      return artist.id !== id;
+    });
+    setArtists(updateArtists);
   }
 
   return (
     <>
-      <h1>Inspiring sculptors:</h1>
-      <input type="text" name="name" onChange={handleNameChange} />{" "}
-      <button onClick={handleButtonClick}>Add</button>
-      <div>
-        <ul>
-          {artists.map((artist) => {
-            return <li key={artist.id}>{artist.name}</li>;
-          })}
-        </ul>
-      </div>
+      <h1>Inspiring sculptors: </h1>
+      <ul>
+        {artists.map((artist) => {
+          return (
+            <li key={artist.id}>
+              {artist.name}{" "}
+              <button onClick={() => handleDeleteButtonClick(artist.id)}>
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 }
