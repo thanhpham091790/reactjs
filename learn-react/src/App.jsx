61502429003b5d866d482129ef1af50991aff2c1
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-08-11 02:31:43 -0600

    Inserting into an array.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,34 +1,42 @@
 import { useState } from "react";
 
-const initialCounters = [0, 0, 0];
+let nextId = 3;
+const initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
 
-export default function CounterList() {
+export default function List() {
   // States
-  const [counters, setCounters] = useState(initialCounters);
+  const [artists, setArtists] = useState(initialArtists);
+  const [name, setName] = useState("");
 
   // Handlers
-  function handleClick(i) {
-    setCounters(
-      counters.map((counter, index) => {
-        if (index === i) {
-          return counter + 1;
-        } else {
-          return counter;
-        }
-      }),
-    );
+  function handleNameChange(e) {
+    setName(e.target.value);
+  }
+  function handleNameInsert() {
+    const insertAt = 1;
+    const nextArtists = [
+      ...artists.slice(0, insertAt),
+      { id: nextId++, name: name },
+      ...artists.slice(insertAt),
+    ];
+    setArtists(nextArtists);
   }
 
   return (
     <>
+      <h1>Inspiring sculptors:</h1>
+      <div>
+        <input type="text" value={name} onChange={handleNameChange} />{" "}
+        <button onClick={handleNameInsert}>Insert</button>
+      </div>
       <ul>
-        {counters.map((counter, index) => {
-          return (
-            <li key={index}>
-              {counter} <button onClick={() => handleClick(index)}>+1</button>
-            </li>
-          );
-        })}
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
       </ul>
     </>
   );
