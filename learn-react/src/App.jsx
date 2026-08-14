af80bda857b0a03e8124675edf25566348877a91
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-08-14 01:16:15 -0600

    When updating nested state, you need to create copies from the point where you want to update, and all the way up to the top level.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,29 +1,57 @@
 import { useState } from "react";
 
 const initialList = [
-  { id: 0, title: "Big Bellies" },
-  { id: 1, title: "Lunar Landscape" },
-  { id: 2, title: "Terracotta Army" },
+  { id: 0, title: "Big Bellies", seen: false },
+  { id: 1, title: "Lunar Landscape", seen: false },
+  { id: 2, title: "Terracotta Army", seen: true },
 ];
 
 export default function List() {
   // States
-  const [list, setList] = useState(initialList);
+  const [myList, setMyList] = useState(initialList);
+  const [yourList, setYourList] = useState(initialList);
 
   // Handlers
-  function handleClick() {
-    const reverseList = [...list];
-    reverseList.reverse();
-    setList(reverseList);
+  function handleMyListClick(itemId, nextSeen) {
+    const myNextList = [...myList];
+    const item = myNextList.find((i) => i.id === itemId);
+    item.seen = nextSeen;
+    setMyList(myNextList);
   }
+
+  function handleYourListClick(itemId, nextSeen) {
+    const yourNextList = [...yourList];
+    const item = yourNextList.find((i) => i.id === itemId);
+    item.seen = nextSeen;
+    setYourList(yourNextList);
+  }
+
   return (
     <>
-      <button onClick={handleClick}>Reverse</button>
-      <ul>
-        {list.map((item, index) => (
-          <li key={index}>{item.title}</li>
-        ))}
-      </ul>
+      <h1>Art Bucket List</h1>
+      <h2>My list of art to see:</h2>
+      <ItemList list={myList} onClick={handleMyListClick}></ItemList>
+      <h2>Your list of art to see:</h2>
+      <ItemList list={yourList} onClick={handleYourListClick}></ItemList>
     </>
+  );
+}
+
+function ItemList({ list, onClick }) {
+  return (
+    <ul>
+      {list.map((item) => (
+        <li key={item.id}>
+          <input
+            type="checkbox"
+            checked={item.seen}
+            onChange={(e) => {
+              onClick(item.id, e.target.checked);
+            }}
+          />{" "}
+          {item.title}
+        </li>
+      ))}
+    </ul>
   );
 }
