56ce01c24a10e40c58aafc602fa5b46a176b420e
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-08-15 21:15:47 -0600

    Remove an item from the shopping cart.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -23,7 +23,7 @@ export default function ShoppingCart() {
   const [products, setProducts] = useState(initialProducts);
 
   // Handlers
-  function handleButtonClick(productId) {
+  function handleAddButtonClick(productId) {
     const updatedProducts = products.map((product) => {
       if (product.id === productId) {
         return { ...product, count: product.count + 1 };
@@ -34,6 +34,20 @@ export default function ShoppingCart() {
     setProducts(updatedProducts);
   }
 
+  function handleMinusButtonClick(productId) {
+    const updatedProducts = products
+      .map((product) => {
+        if (product.id === productId) {
+          return { ...product, count: product.count - 1 };
+        } else {
+          return product;
+        }
+      })
+      .filter((product) => product.count > 0);
+
+    setProducts(updatedProducts);
+  }
+
   // Rendering
   return (
     <>
@@ -41,7 +55,10 @@ export default function ShoppingCart() {
         {products.map((product) => (
           <li key={product.id}>
             {product.name} (<b>{product.count}</b>){" "}
-            <button onClick={() => handleButtonClick(product.id)}>+</button>
+            <button onClick={() => handleAddButtonClick(product.id)}>+</button>{" "}
+            <button onClick={() => handleMinusButtonClick(product.id)}>
+              -
+            </button>
           </li>
         ))}
       </ul>
