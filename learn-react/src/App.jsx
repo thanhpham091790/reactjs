d26a2f1663a2c0de8fc30c6bfe64393cc1ca8528
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-08-27 17:42:18 -0600

    Create form at the beginning status

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,84 +1,31 @@
 import { useState } from "react";
-import AddToDo from "./AddTodo";
-import TodoList from "./TodoList";
 
-const initialTodos = [
-  { id: 0, title: "Buy milk", done: true },
-  { id: 1, title: "Eat tacos", done: false },
-  { id: 2, title: "Brew tea", done: false },
-];
-
-let nextId = 3;
-
-export default function TaskApp() {
+export default function Form() {
   /**
-   * All States
+   * All states
    */
-  const [todos, setTodos] = useState(initialTodos);
-  const [title, setTitle] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [status, setStatus] = useState("typing");
 
   /**
-   * All Handlers
+   * All handlers
    */
 
-  // Handler When change value of input before add it.
-  function handleAddInputChange(event) {
-    setTitle(event.target.value);
+  // Handler when change answer
+  function handleAnswerChange(event) {
+    setAnswer(event.target.value);
   }
 
-  // Handler when click on Add button
-  function handleAddButtonClick() {
-    if (title !== "") {
-      setTodos([...todos, { id: nextId++, title: title, done: false }]);
-    }
-  }
-
-  // Handler when click on checkbox
-  function handleCheckboxChange(todoId) {
-    const newTodos = todos.map((todo) => {
-      if (todo.id === todoId) {
-        return { ...todo, done: event.target.checked };
-      } else {
-        return todo;
-      }
-    });
-    setTodos(newTodos);
-  }
-
-  // Handler when edit input change
-  function handleEditInputChange(todoId) {
-    const newTodos = todos.map((todo) => {
-      if (todo.id === todoId) {
-        return { ...todo, title: event.target.value };
-      } else {
-        return todo;
-      }
-    });
-    setTodos(newTodos);
-  }
-
-  // Handler when click on Delete button
-  function handleDeleteButtonClick(todoId) {
-    const newTodos = todos.filter((todo) => {
-      return todo.id !== todoId;
-    });
-    setTodos(newTodos);
-  }
-
-  // Rendering
   return (
     <>
-      <AddToDo
-        title={title}
-        handleAddInputChange={handleAddInputChange}
-        handleAddButtonClick={handleAddButtonClick}
-      />
-      <TodoList
-        todos={todos}
-        handleCheckboxChange={handleCheckboxChange}
-        handleDeleteButtonClick={handleDeleteButtonClick}
-        handleEditInputChange={handleEditInputChange}
-      />
+      <h1>City quiz</h1>
+      <p>
+        In which city is there a billboard that turns air into drinkable water?
+      </p>
+      <p>
+        <textarea value={answer} onChange={handleAnswerChange}></textarea>
+      </p>
+      <button disabled={status == "typing" ? true : false}>Submit</button>
     </>
   );
 }
