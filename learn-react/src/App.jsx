58a280c3c64732a060ee4336d304b711eb24453e
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-08-27 19:08:51 -0600

    Choosing the state structure.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,18 +1,58 @@
 import { useState } from "react";
 
 export default function Form() {
+  /**
+   * All states
+   */
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  /**
+   * All handlers
+   */
+
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+  }
+
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+  }
+
+  /**
+   * All variable
+   */
+
+  let fullName;
+
+  /**
+   * All logics
+   */
+
+  fullName = firstName + " " + lastName;
+
   return (
     <>
       <h1>Let's check you in</h1>
       <p>
         <label>First name: </label>
-        <input type="text" name="firstname" />
+        <input
+          type="text"
+          name="firstname"
+          value={firstName}
+          onChange={handleFirstNameChange}
+        />
       </p>
       <p>
         <label>Last name: </label>
-        <input type="text" name="lastname" />
+        <input
+          type="text"
+          name="lastname"
+          value={lastName}
+          onChange={handleLastNameChange}
+        />
       </p>
-      <p>Your ticket will be issued to:</p>
+      <p>Your ticket will be issued to: {fullName}</p>
     </>
   );
 }
