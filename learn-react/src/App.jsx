8f25e93162d23f23fa497d633b18883b6fceb6bd
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-08-30 02:58:26 -0600

    At the bottom of the file, the reducer function specifics how the state should update in response to each other.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -2,19 +2,6 @@ import { useReducer } from "react";
 
 const initialState = { count: 0 };
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "add":
-      return { count: state.count + 1 };
-    case "minus":
-      return { count: state.count - 1 };
-    case "reset":
-      return { count: 0 };
-    default:
-      return state;
-  }
-}
-
 export default function Counter() {
   /**
    * All states
@@ -31,4 +18,17 @@ export default function Counter() {
       </div>
     </>
   );
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add":
+      return { count: state.count + 1 };
+    case "minus":
+      return { count: state.count - 1 };
+    case "reset":
+      return { count: 0 };
+    default:
+      return state;
+  }
 }
