768cb048f4f31075306cca11c9ce667f374d0323
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-09-05 15:57:03 -0600

    Visualizing the UI when state is "submitting". The form is completely disabled. Spinner is shown.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -2,16 +2,27 @@ import { useState } from "react";
 
 // The status state can be 'empty', 'typing', 'submitting', 'success', or 'error'
 
-export default function Form({ status = "typing" }) {
+export default function Form({ status = "submitting" }) {
   // All variables
-  let disabled;
+  let isButtonDisabled = true,
+    isTextareaDisabled = false,
+    isSpinerHidden = true,
+    spinerStyles = {};
 
   if (status === "empty") {
-    disabled = true;
+    isButtonDisabled = true;
   }
   if (status === "typing") {
-    disabled = false;
+    isButtonDisabled = false;
   }
+
+  if (status === "submitting") {
+    isButtonDisabled = true;
+    isTextareaDisabled = true;
+    isSpinerHidden = false;
+  }
+
+  spinerStyles = isSpinerHidden ? { display: "none" } : { display: "block" };
 
   return (
     <>
@@ -20,9 +31,16 @@ export default function Form({ status = "typing" }) {
         In which city is there a billboard that turns air into drinkable water?
       </p>
       <p>
-        <textarea></textarea>
+        <textarea disabled={isTextareaDisabled}></textarea>
         <br />
-        <button disabled={disabled}>Submit</button>
+        <button disabled={isButtonDisabled}>Submit</button>
+        <br />
+        <img
+          style={spinerStyles}
+          src="https://cdn.pixabay.com/animation/2023/08/11/21/18/21-18-05-265_512.gif"
+          width="30px"
+          height="30px"
+        />
       </p>
     </>
   );
