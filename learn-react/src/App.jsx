46cdfdbae26e4ce85713e9058c4470f560b437b0
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-09-10 17:44:43 -0600

    Remove any non-essential state variables.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -6,12 +6,9 @@ export default function Form({ status = "empty" }) {
   // All states
   const [answer, setAnswer] = useState("");
   const [error, setError] = useState(null);
-
-  // const [isEmpty, setIsEmpty] = useState(true);
   const [isTyping, setIsTyping] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isSuccess, setIsSuccess] = useState(false);
-  // const [isError, setIsError] = useState(false);
 
   // All variables
   let isButtonDisabled = true,
