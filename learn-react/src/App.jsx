bb871ef34fb005f265d0f915ec4dc0f361b43f76
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-09-12 13:56:46 -0600

    Create UI for view state

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,87 +1,22 @@
-import { useState } from "react";
-
-// The status state can be 'empty', 'typing', 'submitting', 'success', or 'error'
-
-export default function Form() {
-  /**
-   *  All states
-   */
-  const [answer, setAnswer] = useState("");
-  const [error, setError] = useState(null);
-
-  // The status state can be 'typing', submitting, or 'success'
-  const [status, setStatus] = useState("typing");
-
-  /**
-   *  All handlers
-   */
-  function handleTextareaChange(e) {
-    setAnswer(e.target.value);
+export default function EditProfile({ state = "view" }) {
+  if (state === "view") {
+    return (
+      <>
+        <p>
+          First name: <b>Jane</b>
+        </p>
+        <p>
+          Last name: <b>Jacobs</b>
+        </p>
+        <p>
+          <button>Edit Profile</button>
+        </p>
+        <p>
+          <i>Hello, Jane Jacobs!</i>
+        </p>
+      </>
+    );
+  } else {
+    return <>Edit</>;
   }
-
-  async function handleSubmitButtonClick(e) {
-    e.preventDefault();
-    setStatus("submitting");
-    try {
-      await submitForm(answer);
-      setStatus("success");
-    } catch (err) {
-      setStatus("typing");
-      setError(err);
-    }
-  }
-
-  function submitForm() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        let shouldError = answer.toLocaleLowerCase() !== "lima";
-        if (shouldError) {
-          reject(new Error("Good guess but a wrong answer. Try again!"));
-        } else {
-          resolve();
-        }
-      }, 1500);
-    });
-  }
-
-  return (
-    <>
-      {status === "success" ? (
-        <h1>Thank you</h1>
-      ) : (
-        <>
-          <h1>City quiz</h1>
-          <p>
-            In which city is there a billboard that turns air into drinkable
-            water?
-          </p>
-          <p>
-            <textarea
-              disabled={status === "submitting"}
-              onChange={handleTextareaChange}
-            ></textarea>
-            <br />
-            <button
-              disabled={answer === "" || status !== "typing"}
-              onClick={handleSubmitButtonClick}
-            >
-              Submit
-            </button>
-            <br />
-            <img
-              style={{ display: status === "submitting" ? "block" : "none" }}
-              src="https://loading.io/assets/mod/spinner/spinner/sample.gif"
-              width="100px"
-              height="100px"
-            />
-          </p>
-          <h2
-            style={{ color: "red", display: error === null ? "none" : "block" }}
-          >
-            {error !== null && error.message}
-          </h2>
-        </>
-      )}
-    </>
-  );
 }
