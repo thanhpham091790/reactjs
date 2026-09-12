f447712165704e5839d8c878b32b92f7d34da6ff
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-09-12 17:30:24 -0600

    Implementing UI when state is "typing"

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,47 +1,16 @@
 import { useState } from "react";
 
-export default function MovingDot() {
-  /**
-   * All states
-   */
-  const [position, setPosition] = useState({ x: 8, y: 8 });
-
-  /**
-   * All handlers
-   */
-
-  function handlePointerMove(e) {
-    setPosition({
-      x: e.clientX,
-      y: e.clientY,
-    });
+export default function FeedbackForm({ status = "typing" }) {
+  if (status === "typing") {
+    return (
+      <>
+        <p>How was your stay at The Prancing Pony?</p>
+        <p>
+          <textarea />
+          <br />
+          <button>Send</button>
+        </p>
+      </>
+    );
   }
-
-  return (
-    <>
-      <div
-        style={{
-          position: "relative",
-          width: "calc(100vw - 18px)",
-          height: "calc(100vh - 18px)",
-          border: "1px solid lightgrey",
-        }}
-        onPointerMove={handlePointerMove}
-      >
-        <div
-          style={{
-            position: "absolute",
-            backgroundColor: "green",
-            width: 16,
-            height: 16,
-            borderRadius: "50%",
-
-            left: -8,
-            top: -8,
-            transform: `translate(${position.x - 8}px, ${position.y - 8}px)`,
-          }}
-        />
-      </div>
-    </>
-  );
 }
