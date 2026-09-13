65ee75835f085c3970c90a55296cf29320307a39
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-09-12 18:16:46 -0600

    Connect event handler to set states.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -7,19 +7,46 @@ export default function FeedbackForm() {
   const [text, setText] = useState("");
   const [status, setStatus] = useState("typing"); // "typing", "sending", "sent"
 
+  /**
+   * All handlers
+   */
+
+  function handleTextareaChange(e) {
+    setText(e.target.value);
+  }
+
+  async function handleSendButtonClick(e) {
+    e.preventDefault();
+    setStatus("sending");
+    await sendFeedback(text);
+    setStatus("sent");
+  }
+
   if (status === "sent") return <h1>Thanks for feedback!</h1>;
 
   return (
     <>
       <p>How was your stay at The Prancing Pony?</p>
       <p>
-        <textarea disabled={status === "sending"} />
+        <textarea
+          disabled={status === "sending"}
+          value={text}
+          onChange={handleTextareaChange}
+        />
         <br />
-        <button disabled={status === "sending"}>Send</button>
+        <button disabled={status === "sending"} onClick={handleSendButtonClick}>
+          Send
+        </button>
       </p>
       <p style={{ display: status === "sending" ? "block" : "none" }}>
         Sending...
       </p>
     </>
   );
+}
+
+function sendFeedback(text) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
 }
