f50572a4524b56acbe165752354d0aeca4a852b7
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-09-13 00:24:54 -0600

    Avoid duplication in state.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,35 +1,61 @@
 import { useState } from "react";
 
-export default function Form() {
+const initialItems = [
+  { title: "pretzels", id: 0 },
+  { title: "crispy seaweed", id: 1 },
+  { title: "granola bar", id: 2 },
+];
+
+export default function Menu() {
   /**
    * All states
    */
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [items, setItems] = useState(initialItems);
+  const [selectedId, setSelectedId] = useState(0);
+
+  /**
+   * All handlers
+   */
+  function handleInputChange(id) {
+    const updatedItems = items.map((item) => {
+      if (item.id === id) {
+        return { ...item, title: event.target.value };
+      } else {
+        return item;
+      }
+    });
+
+    setItems(updatedItems);
+  }
+
+  function handleChooseButtonClick(id) {
+    setSelectedId(id);
+  }
+
+  /**
+   * All variables
+   */
+  const selectedItem = items.filter((item) => item.id === selectedId)[0];
 
   return (
     <>
-      <h3>Let's check you in</h3>
-      <div>
-        <p>
-          First name:{" "}
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </p>
-        <p>
-          Last name:{" "}
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </p>
-      </div>
+      <h2>What's your travel snack?</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <input
+              type="text"
+              value={item.title}
+              onChange={() => handleInputChange(item.id)}
+            />{" "}
+            <button onClick={() => handleChooseButtonClick(item.id)}>
+              Choose
+            </button>
+          </li>
+        ))}
+      </ul>
       <p>
-        Your ticket will be issued to: {firstName} {lastName}
+        You picked <b>{selectedItem.title}</b>
       </p>
     </>
   );
