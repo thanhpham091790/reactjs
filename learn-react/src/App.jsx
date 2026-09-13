dc7d10e307444282b93688facd8618422b2a6ce4
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2026-09-12 18:00:55 -0600

    Model states using useState.

--- learn-react/src/App.jsx
+++ learn-react/src/App.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react";
 
-export default function FeedbackForm({ status = "typing" }) {
-  if (status === "typing") {
-    return (
-      <>
-        <p>How was your stay at The Prancing Pony?</p>
-        <p>
-          <textarea />
-          <br />
-          <button>Send</button>
-        </p>
-      </>
-    );
-  }
+export default function FeedbackForm() {
+  /**
+   * All states
+   */
+  const [text, setText] = useState("");
+  const [status, setStatus] = useState("typing"); // "typing", "sending", "sent"
+
+  if (status === "sent") return <h1>Thanks for feedback!</h1>;
+
+  return (
+    <>
+      <p>How was your stay at The Prancing Pony?</p>
+      <p>
+        <textarea disabled={status === "sending"} />
+        <br />
+        <button disabled={status === "sending"}>Send</button>
+      </p>
+      <p style={{ display: status === "sending" ? "block" : "none" }}>
+        Sending...
+      </p>
+    </>
+  );
 }
